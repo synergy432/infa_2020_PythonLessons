1e6194ad4f18a86130bc1632a832a9a72e5fc0ac
Linar <Li.nar.85@mail.ru>
2020-10-01 16:06:55 +0300

    changed name and new file by name 432

---
+++ my trying.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2
+def car1(x,y):
+    print("hiiiiii")
+    pass
+def car2(x,y):
+    print("fiiiiii")
+    pass
+def car3(x,y):
+    print("siiiiii")
+    pass
+
+
+car1(1, 2)
+car2(x, y)
+car3(x, y)
